180ab3acf4aee148864fe780407af01670960e38
jeongmyeong <kimbym23@gmail.com>
2022-02-02 15:43:22 +0900

    initial commit

---
+++ supplier/src/main/kotlin/com/example/supplier/SupplierApplication.kt
@@ -0,0 +1,11 @@
+package com.example.supplier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupplierApplication
+
+fun main(args: Array<String>) {
+    runApplication<SupplierApplication>(*args)
+}
